3abb700dd9cf168994bca57ef0194bce8df11f9a
Zodiark619 <seonghyejin33@gmail.com>
2022-11-20 07:51:52 +0700

    hackerrank warmip

---
+++ hackerrank/warmup/CompareTheTriplets/Program.cs
@@ -0,0 +1,37 @@
+﻿
+static List<int> CompareTriplets(List<int> a,List<int> b){
+
+
+
+int alice=0;
+int bob=0;
+for(int i=0;i<a.Count;i++){
+    if(a[i]<b[i]){
+        bob++;
+    }else if(a[i]>b[i]){
+        alice++;
+    }
+}
+List<int> meong=new(){alice,bob};
+return meong;
+
+
+
+}
+
+List<int> a=new(){17,28,30};
+List<int> b=new(){99,16,8};
+Console.WriteLine("Input 1: ");
+foreach(var i in a){
+    Console.Write(i);
+    Console.Write(" ");
+}
+Console.WriteLine();
+Console.WriteLine("Input 2: ");
+foreach(var i in b){
+    Console.Write(i);
+    Console.Write(" ");
+}
+Console.WriteLine();
+
+Console.WriteLine($"Alice: {CompareTriplets(a,b)[0]} - Bob: {CompareTriplets(a,b)[1]}");
---
+++ hackerrank/warmup/SimpleArraySum/Program.cs
@@ -0,0 +1,18 @@
+﻿
+static int SimpleArraySum(List<int> arr){
+int total=0;
+foreach(var i in arr){
+    total+=i;
+}
+return total;
+}
+
+
+List<int> meong=new(){1,2,3,4,10,11};
+Console.Write("Input: ");
+foreach(var i in meong){
+Console.Write(i+" ");
+
+}
+Console.WriteLine();
+Console.WriteLine($"Output: {SimpleArraySum(meong)}");
