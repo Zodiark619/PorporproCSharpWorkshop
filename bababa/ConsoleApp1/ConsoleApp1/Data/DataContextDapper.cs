88cedb11b7d8b14cd18dcbe93f77249255c8fda3
Zodiark619 <seonghyejin33@gmail.com>
2022-12-01 16:14:56 +0700

    asdasd

---
+++ bababa/ConsoleApp1/ConsoleApp1/Data/DataContextDapper.cs
@@ -0,0 +1,32 @@
+using System.Data;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace ConsoleApp1.Data{
+    public class DataContextDapper{
+       private string _connectionString="Server=localhost;Database=DotNetCourseDatabase;Trusted_Connection=true;TrustServerCertificate=true;";
+
+public IEnumerable<T> LoadData<T>(string sql){
+IDbConnection dbConnection=new SqlConnection(_connectionString);
+return dbConnection.Query<T>(sql);
+
+}
+public T LoadDataSingle<T>(string sql){
+IDbConnection dbConnection=new SqlConnection(_connectionString);
+return dbConnection.QuerySingle<T>(sql);
+
+}
+
+public bool ExecuteSql(string sql){
+IDbConnection dbConnection=new SqlConnection(_connectionString);
+return (dbConnection.Execute(sql)>0);
+
+}
+public int ExecuteSqlWithRowCount(string sql){
+IDbConnection dbConnection=new SqlConnection(_connectionString);
+return dbConnection.Execute(sql);
+
+}
+
+    }
+}
--- bababa/ConsoleApp1/ConsoleApp1/Program.cs
+++ bababa/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,51 +1,58 @@
-﻿Cat sss1 = new() { Name = "ANSajskdn", Race = "White", Age = 21 };
-Cat sss2 = new() { Name = "cbgfbfg", Race = "Black", Age = 2 };
-Cat sss3 = new() { Name = "4354353", Race = "Black", Age = 17 };
-Cat sss4 = new() { Name = "kk", Race = "White", Age = 5 };
+﻿
+using System.Data;
+using System.Globalization;
+using ConsoleApp1.Data;
+using ConsoleApp1.Models;
+using Dapper;
+using Microsoft.Data.SqlClient;
 
-List<Cat> kucing = new()
-{
-    sss1,sss2,sss3,sss4
+DataContextDapper dapper=new DataContextDapper();
+DateTime rightNow=dapper.LoadDataSingle<DateTime>("select getdate()");
+Computer myComputer=new(){
+    Motherboard="Z690",
+    HasWifi=true,
+    HasLTE=false,
+    ReleaseDate=DateTime.Now,
+    Price=943.87m,
+    VideoCard="RTX 2060"
 };
-Meongzz("Kitten",kucing,Kitten);
-Console.WriteLine();
-Meongzz("Adult", kucing, Adult);
-Console.WriteLine();
-Meongzz("White", kucing, White);
-static bool Kitten(Cat cat)
-{
-    return cat.Age < 10;
-}
-static bool Adult(Cat cat)
-{
-    return cat.Age > 10;
-}
-static bool White(Cat cat)
-{
-    return cat.Race == "White";
-}
-static void Meongzz(string title, List<Cat> cats, Kucingzz kucingzz)
-{
-    Console.WriteLine(title);
-    foreach(var cat in cats)
-    {
-        if (kucingzz(cat))
-        {
-            Console.WriteLine(cat.Name+" "+cat.Race+" "+cat.Age);
+string sql=@"insert into tutorialappschema.computer (
+    Motherboard,
+    HasWifi,
+    HasLTE,
+    ReleaseDate,
+    Price,
+    VideoCard
+    ) values(
+        '" +myComputer.Motherboard 
+        + "','"+myComputer.HasWifi
+        + "','"+myComputer.HasLTE
+        + "','"+myComputer.ReleaseDate
+        + "','"+myComputer.Price.ToString("0.00",CultureInfo.InvariantCulture)
+        + "','"+myComputer.VideoCard
+      +  "')";
+      System.Console.WriteLine(sql);
+      bool result=dapper.ExecuteSql(sql);
+      System.Console.WriteLine( result);
 
-        }
-    }
+      string sqlSelect=@"select
+       computer.Motherboard,
+    computer.HasWifi,
+    computer.HasLTE,
+    computer.ReleaseDate,
+    computer.Price,
+    computer.VideoCard
+     from tutorialappschema.computer";
+IEnumerable<Computer> computers=dapper.LoadData<Computer>(sqlSelect);
+      System.Console.WriteLine("'Motherboard','HasWifi','HasLTE','ReleaseDate','Price','VideoCard'");
 
-   
-
-
+foreach(Computer singleComputer in computers){
+    System.Console.WriteLine("'" +singleComputer.Motherboard 
+        + "','"+singleComputer.HasWifi
+        + "','"+singleComputer.HasLTE
+        + "','"+singleComputer.ReleaseDate
+        + "','"+singleComputer.Price
+        + "','"+singleComputer.VideoCard
+      +  "'");
 }
-public class Cat
-{
-    public string Name { get; set; }
-    public string Race { get; set;}
-    public int Age { get; set; }
-}
-
-public delegate bool Kucingzz(Cat cat);
 
